8120ab3caed58013225e8c4f6f7b7157963522e7
Peter Monks <pmonks@gmail.com>
2020-07-01 16:23:04 -0700

    :new: Fix issue #29

--- src/futbot/football_data.clj
+++ src/futbot/football_data.clj
@@ -28,6 +28,8 @@
 (def endpoint-matches-on-date "/v2/matches/?dateFrom=%s&dateTo=%s")
 (def endpoint-match-details   "/v2/matches/%d")
 
+(def maximum-retries 3)
+
 (defn clojurise-json-key
   "Converts JSON string keys (e.g. \"fullName\") to Clojure keyword keys (e.g. :full-name)."
   [k]
@@ -39,16 +41,33 @@
       "_"
       "-")))
 
+(defn parse-int
+  [s]
+  (try
+    (Integer/parseInt s)
+    (catch NumberFormatException nfe
+      nil)))
+
 (defn football-data-api-call
-  "Calls the given football-data API endpoint using the given token, parses the result, and returns it.  Throws an ex-info if an error occurs (map contains HTTP :status)."
-  [endpoint token]
-  (let [api-url                     (str api-host endpoint)
-        options                     {:headers {"X-Auth-Token" token}}
-        {:keys [status error body]} @(http/get api-url options)]
-    (if error
-      (throw (ex-info  (format "football-data API call (%s) failed" api-url) {:status status} error))
-      (ch/parse-string body
-                       clojurise-json-key))))
+  "Calls the given football-data API endpoint using the given token, parses the result and returns it.  Throws an ex-info if an error occurs (the ex-info map contains HTTP :status)."
+  [token endpoint]
+  (let [api-url (str api-host endpoint)
+        options {:headers {"X-Auth-Token" token}}]
+    (loop [attempt 1]
+      (log/debug "Calling" api-url)
+      (let [{:keys [status headers body error]} @(http/get api-url options)]
+        (if (= status 429)                   ; We got throttled
+          (if (> attempt maximum-retries)    ; Bail out after too many retry attempts
+            (throw (ex-info (format "Too many retries (%d) to football-data API %s" attempt api-url) {:status status} error))
+            (let [counter-reset-ms (* 1000 (parse-int (s/trim (get headers "X-RequestCounter-Reset"))))  ; Find out how long football-data wants us to wait
+                  retry-in-ms      (+ counter-reset-ms (rand-int (* 1000 attempt)))]            ; Add some randomness to try to avoid stampeding herds
+              (log/warn (format "football-data API call attempt %d throttled, waiting %dms before retrying." attempt retry-in-ms))
+              (Thread/sleep retry-in-ms)
+              (recur (inc attempt))))
+          (if error
+            (throw (ex-info (format "football-data API call (%s) failed" api-url) {:status status} error))
+            (ch/parse-string body
+                             clojurise-json-key)))))))
 
 (defn matches-on-day
   "Returns a list of all matches visible to the given token, on the given day (defaults to today UTC if not otherwise specified), sorted by scheduled time and then by competition."
@@ -56,9 +75,8 @@
   ([token day]
     (let [day-as-string (tm/format "yyyy-MM-dd" day)
           api-call      (format endpoint-matches-on-date day-as-string day-as-string)]
-      (log/debug (str "Calling " api-call))
       (sort-by #(str (:utc-date %) "-" (:name (:competition %)))
-               (:matches (football-data-api-call api-call token))))))
+               (:matches (football-data-api-call token api-call))))))
 
 (defn scheduled-matches-on-day
   "Returns a list of all scheduled matches visible to the given token, on the given day (defaults to today UTC if not otherwise specified), sorted by scheduled time and then by competition."
@@ -68,4 +86,4 @@
 (defn match
   "Returns match details for a single match."
   [token match-id]
-  (football-data-api-call (format endpoint-match-details match-id) token))
+  (football-data-api-call token (format endpoint-match-details match-id)))
